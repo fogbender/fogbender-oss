6c47024b86eb6213804dbc68da9760792c920f2b
JLarky <jlarky@gmail.com>
2020-04-17 07:49:50 -0700

    pass userHash to elixir, does nothing right now

--- packages/fogbender-react/src/index.tsx
+++ packages/fogbender-react/src/index.tsx
@@ -14,6 +14,7 @@ export type Token = {
   customerExternalId: string;
   customerName: string;
   userExternalId: string;
+  userHash?: string;
   userName: string;
   userPicture?: string;
   userEmail?: string;
