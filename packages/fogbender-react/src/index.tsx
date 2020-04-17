f2a4878c519949299b45a4db69e2888decbde3c5
JLarky <jlarky@gmail.com>
2020-04-17 08:03:23 -0700

    clean up unused

--- packages/fogbender-react/src/index.tsx
+++ packages/fogbender-react/src/index.tsx
@@ -1,13 +1,4 @@
 import * as React from "react";
-import styles from "./styles.module.css";
-
-interface Props {
-  text: string;
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>;
-};
 
 export type Token = {
   widgetId: string;
