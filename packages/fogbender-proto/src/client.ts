49f1564231ab9a8a2383bf3a181cdbc95be205f8
Nikolai Gromin <gromin@gmail.com>
2021-12-07 20:18:23 +0300

    fogbender-proto: allow to pass "serverApiUrl" config in "client" param

--- packages/fogbender-proto/src/client.ts
+++ packages/fogbender-proto/src/client.ts
@@ -6,6 +6,7 @@ export type ErrorKind = "server_stopped_responding" | "other";
 
 export interface Client {
   getEnv?(): Env | undefined;
+  getServerApiUrl?(): string | undefined;
   onError?(type: ErrorType, kind: ErrorKind, ...errors: (Error | string)[]): void;
   setSession?(sessionId: string, userId?: string, helpdeskId?: string): void;
   onWrongToken?(token: AnyToken): void;
