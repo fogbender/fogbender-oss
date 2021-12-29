73adeafaf0929338ef943f987120c7d07e7f4c61
JLarky <jlarky@gmail.com>
2021-12-28 17:20:31 -0700

    separate types.ts

--- packages/fogbender/src/index.ts
+++ packages/fogbender/src/index.ts
@@ -1,37 +1,6 @@
 import { createFloatingWidget } from "./floatingWidget";
-
-// make sure to keep in sync with fogbender-ptoto schema
-export type Token = {
-  widgetId: string;
-  customerId: string;
-  customerName: string;
-  userId: string;
-  userHMAC?: string;
-  userJWT?: string;
-  userPaseto?: string;
-  userName: string;
-  userAvatarUrl?: string;
-  userEmail?: string;
-};
-
-export type Badge = {
-  count: number;
-  mentionsCount: number;
-  roomId: string;
-};
-
-export type Fogbender = (opts: {
-  rootEl?: HTMLElement;
-  url?: string;
-  token?: Token;
-  headless?: boolean;
-  onBadges?: (badges: Badge[]) => void;
-  showFloatingWidget?: boolean;
-}) => void;
-
-export interface NewFogbenderType {
-  createFloatingWidget(rootEl: HTMLElement, url: string, token: Token): Promise<NewFogbenderType>;
-}
+import type { Token, Badge, NewFogbenderType, Fogbender } from "./types";
+export type { Token, Badge, NewFogbenderType, Fogbender };
 
 export const createNewFogbender = (): NewFogbenderType => {
   const fogbender: NewFogbenderType = {
---
+++ packages/fogbender/src/types.ts
@@ -0,0 +1,32 @@
+// make sure to keep in sync with fogbender-ptoto schema
+export type Token = {
+  widgetId: string;
+  customerId: string;
+  customerName: string;
+  userId: string;
+  userHMAC?: string;
+  userJWT?: string;
+  userPaseto?: string;
+  userName: string;
+  userAvatarUrl?: string;
+  userEmail?: string;
+};
+
+export type Badge = {
+  count: number;
+  mentionsCount: number;
+  roomId: string;
+};
+
+export type Fogbender = (opts: {
+  rootEl?: HTMLElement;
+  url?: string;
+  token?: Token;
+  headless?: boolean;
+  onBadges?: (badges: Badge[]) => void;
+  showFloatingWidget?: boolean;
+}) => void;
+
+export interface NewFogbenderType {
+  createFloatingWidget(rootEl: HTMLElement, url: string, token: Token): Promise<NewFogbenderType>;
+}
