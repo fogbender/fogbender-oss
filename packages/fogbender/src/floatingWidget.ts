8c78d2209c6926f3e09e4281d50f23dc5f4f1e6f
Nikolai Gromin <gromin@gmail.com>
2021-12-30 23:05:52 +0300

    Adjust floating widget design

--- packages/fogbender/src/floatingWidget.ts
+++ packages/fogbender/src/floatingWidget.ts
@@ -9,29 +9,25 @@ function on<T>(element: HTMLElement, event: string, callback: (data: CustomEvent
 export function createFloatingWidget(rootEl: HTMLElement, url: string, token: Token) {
   const container = document.createElement("div");
   container.style.position = "fixed";
-  container.style.width = "60px";
-  container.style.height = "60px";
   container.style.bottom = "0";
   container.style.right = "0";
-  container.style.backgroundColor = "white";
   container.style.zIndex = "9999";
-  container.style.margin = "0 20px 20px 0";
-  container.style.boxShadow = "0 2px 10px 1px #b5b5b5";
-  container.style.borderRadius = "60px";
   const button = document.createElement("button");
-  button.style.display = "flex";
-  button.style.flexDirection = "column";
-  button.style.width = "60px";
-  button.style.height = "60px";
-  container.style.borderRadius = "60px";
-  button.style.justifyContent = "center";
-  button.style.alignItems = "center";
+  button.style.display = "block";
+  button.style.outline = "none";
   button.innerHTML = `
-    <svg width="45" height="45" viewBox="0 0 83 99" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M17.8439 0L0 60.4379L17.8439 98.1693L36.6156 60.4379L17.8439 0Z" fill="#FE346E"/>
-  <path d="M32.3394 79.8936L40.9807 98.1659L59.2716 61.4007L40.9807 0L32.3623 30.4349L41.8109 60.8557L32.3394 79.8936Z" fill="#FF7315"/>
-  <path d="M55.474 79.8937L64.1153 98.1659L82.4062 61.4007L64.1153 0L55.3008 31.1277L64.4521 61.8477L55.474 79.8937Z" fill="#7E0CF5"/>
-  </svg>`;
+    <svg width="120" height="120" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#a)"><path d="M87.026 33.083c-7.775-9.336-11.662-14.005-15.981-16.273a24.514 24.514 0 0 0-23.33.29c-4.26 2.374-8.055 7.167-15.643 16.753-6.296 7.953-9.443 11.93-10.805 16a24.514 24.514 0 0 0 2.43 20.724c2.266 3.645 6.224 6.767 14.14 13.01 6.218 4.904 9.327 7.356 12.643 8.676a24.515 24.515 0 0 0 17.568.215c3.346-1.238 6.538-3.63 12.922-8.417 8.516-6.385 12.774-9.577 15.167-13.318a24.515 24.515 0 0 0 2.493-21.29c-1.464-4.193-4.844-8.252-11.604-16.37Z" fill="#fff"/><mask id="b" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="20" y="14" width="81" height="80"><path d="M87.026 33.083c-7.775-9.336-11.662-14.005-15.981-16.273a24.514 24.514 0 0 0-23.33.29c-4.26 2.374-8.055 7.167-15.643 16.753-6.296 7.953-9.443 11.93-10.805 16a24.514 24.514 0 0 0 2.43 20.724c2.266 3.645 6.224 6.767 14.14 13.01 6.218 4.904 9.327 7.356 12.643 8.676a24.515 24.515 0 0 0 17.568.215c3.346-1.238 6.538-3.63 12.922-8.417 8.516-6.385 12.774-9.577 15.167-13.318a24.515 24.515 0 0 0 2.493-21.29c-1.464-4.193-4.844-8.252-11.604-16.37Z" fill="#FE346E"/></mask><g mask="url(#b)"><path d="M48.094 29 39 59.803l9.094 19.23 9.568-19.23L48.094 29Z" fill="#FE346E"/><path d="m55.481 69.72 4.404 9.312 9.323-18.738L59.885 29l-4.392 15.512 4.815 15.504-4.827 9.703Z" fill="#FF7315"/><path d="m67.273 69.72 4.405 9.312L81 60.294 71.678 29l-4.493 15.865 4.664 15.657-4.576 9.197Z" fill="#7E0CF5"/></g></g><defs><filter id="a" x="0" y="0" width="120" height="120" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset dy="6"/><feGaussianBlur stdDeviation="10"/><feColorMatrix type="matrix" values="0 0 0 0 0.0751563 0 0 0 0 0.113891 0 0 0 0 0.4625 0 0 0 0.15 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2573_3255"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2573_3255" result="shape"/></filter></defs></svg>`;
+  const badgesCounter = document.createElement("div");
+  badgesCounter.style.display = "none";
+  badgesCounter.style.position = "absolute";
+  badgesCounter.style.top = "20px";
+  badgesCounter.style.right = "25px";
+  badgesCounter.style.padding = "0px 5px";
+  badgesCounter.style.borderRadius = "9999px";
+  badgesCounter.style.background = "#FA3541";
+  badgesCounter.style.color = "#FFF";
+  badgesCounter.style.fontSize = "12px";
+  badgesCounter.innerHTML = "!";
   let chatWindow: null | Window = null;
   button.onclick = () => {
     if (!chatWindow || chatWindow.closed) {
@@ -42,27 +38,14 @@ export function createFloatingWidget(rootEl: HTMLElement, url: string, token: To
     }
     chatWindow?.focus();
   };
-  const style = document.createElement("style");
-  style.innerHTML = `
-    @-webkit-keyframes "fogbender-blink" {
-      from, to {
-        background-color: #fff;
-      }
-      50% {
-        background-color: #ff000030;
-      }
-    }
-    .has-badges {
-      animation: 1s fogbender-blink ease infinite;
-    }`;
   const body = document.getElementsByTagName("body")[0];
   container.appendChild(button);
-  container.appendChild(style);
+  button.appendChild(badgesCounter);
   body.appendChild(container);
 
   on<{ badges: { [roomId: string]: Badge } }>(rootEl, "fogbender.badges", e => {
     const unreadCount = Object.values(e.detail.badges).reduce((acc, b) => acc + b.count, 0);
-    container.classList.toggle("has-badges", unreadCount > 0);
-    // container.style.backgroundColor = unreadCount > 0 ? "rgba(255, 0, 0, 0.5)" : "white";
+    badgesCounter.innerHTML = unreadCount.toString();
+    badgesCounter.style.display = unreadCount > 0 ? "block" : "none";
   });
 }
