07dac72370432db6ba758f1cf3cd1c5e6cfe1540
JLarky <jlarky@gmail.com>
2022-01-28 16:58:05 -0700

    let's introduce server build that does nothing
    - this is the way we can support server side rendering frameworks like remix and next.js

---
+++ packages/fogbender/src/server.ts
@@ -0,0 +1,58 @@
+import type { Token, Badge, NewFogbenderType, FogbenderLoader, Snapshot } from "./types";
+export type { Token, Badge, NewFogbenderType, FogbenderLoader, Snapshot };
+
+export const createNewFogbender = (): NewFogbenderType => {
+  const state = {
+    versions: {} as { [key: string]: string },
+    token: undefined as Token | undefined,
+    url: undefined as string | undefined,
+    iframe: undefined as HTMLIFrameElement | undefined,
+    chatWindow: null as null | Window,
+  };
+  const fogbender: NewFogbenderType & { _privateData: any } = {
+    _privateData: state,
+    async setVersion(tag, version) {
+      state.versions[tag] = version;
+      return fogbender;
+    },
+    async setClientUrl(_url: string) {
+      state.url = _url;
+      return fogbender;
+    },
+    async setToken(token) {
+      state.token = token;
+      if (state.token) {
+        state.token = {
+          ...state.token,
+          versions: { ...state.token.versions, ...state.versions, fogbender: "0.0.1" },
+        };
+      }
+      return fogbender;
+    },
+    async isClientConfigured() {
+      return undefined as any;
+    },
+    async createFloatingWidget() {
+      if (!state.url) {
+        throw new Error("Fogbender: no url given");
+      }
+      if (!state.token) {
+        throw new Error("Fogbender: no token given");
+      }
+      return () => {};
+    },
+    async renderIframe() {
+      if (!state.url) {
+        throw new Error("Fogbender: no url given");
+      }
+      if (!state.token) {
+        throw new Error("Fogbender: no token given");
+      }
+      return () => {};
+    },
+    async renderUnreadBadge() {
+      return () => {};
+    },
+  };
+  return fogbender;
+};
