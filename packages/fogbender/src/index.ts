b18cb72e13a7a7d2582bbf6484e57c88dfd433e2
JLarky <jlarky@gmail.com>
2022-02-05 14:01:02 -0700

    bump fogbender@0.0.5

--- packages/fogbender/src/index.ts
+++ packages/fogbender/src/index.ts
@@ -43,7 +43,7 @@ export const createNewFogbender = (): Fogbender => {
       if (state.token) {
         state.token = {
           ...state.token,
-          versions: { ...state.token.versions, ...state.versions, fogbender: "0.0.4" },
+          versions: { ...state.token.versions, ...state.versions, fogbender: "0.0.5" },
         };
       }
       updateConfigured();
