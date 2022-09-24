1999b0b3f1e698d146a1e3cb7abf179887126c7a
karan-nakra <karannakra1@gmail.com>
2022-09-25 03:20:12 +0530

    feat(fogebender-element): improved fogbender simple widget

---
+++ packages/fogbender-element/src/FogbenderSimpleWidget.tsx
@@ -0,0 +1,31 @@
+import { consume, ICustomElement } from "component-register";
+import { Env, Fogbender, Token } from "fogbender";
+import { customElement } from "solid-element";
+import { createEffect } from "solid-js";
+import { fogbenderContext } from "./FogbenderProvider";
+import { addVersion, renderIframe } from "./utils";
+
+interface SimpleWidgetProps {
+  env?: Env;
+  token?: Token;
+  clientUrl?: string;
+}
+
+customElement<SimpleWidgetProps>(
+  "fogbender-simple-widget",
+  { env: undefined, clientUrl: undefined, token: undefined },
+  (props: SimpleWidgetProps, { element }: { element: ICustomElement }) => {
+    let divRef: HTMLDivElement | undefined;
+
+    const fogbender: Fogbender = consume(fogbenderContext);
+
+    createEffect(() => {
+      fogbender.setClientUrl(props.clientUrl);
+      fogbender.setEnv(props.env);
+      fogbender.setToken(addVersion(props.token));
+      renderIframe(divRef, false, element);
+    });
+
+    return <div ref={divRef}></div>;
+  }
+);
