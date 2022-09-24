e0196a0aa6c5866fc46479c086316c9de110f313
karan-nakra <karannakra1@gmail.com>
2022-09-25 03:22:57 +0530

    feat(fogbender-element): removed solid-js context,provider logic  and implemented context,provider logic of component-register package

--- packages/fogbender-element/src/FogbenderProvider.tsx
+++ packages/fogbender-element/src/FogbenderProvider.tsx
@@ -1,25 +1,32 @@
+import { createContext, provide } from "component-register";
 import { createNewFogbender, Fogbender } from "fogbender";
-import type { JSX, Accessor, Component } from "solid-js";
-import { createContext, useContext, createSignal, createEffect } from "solid-js";
+import { customElement } from "solid-element";
+import type { JSX } from "solid-js";
 
 export interface FogbenderProviderProps {
-  fogbender?: Accessor<Fogbender>;
-  children?: JSX.Element;
+  fogbender?: Fogbender;
+  children?: JSX.Element[];
 }
 
-export const FogbenderContext = createContext<Accessor<Fogbender>>();
+interface Context {
+  id: symbol;
+  initFn: Function;
+}
 
-export function getFogbender() {
-  const fogbender = useContext(FogbenderContext);
-
-  if (!fogbender) {
-    throw new Error("No fogbender set, use FogbenderProvider to set one");
-  }
+const fogbenderContext: Context = createContext((fogbender?: Fogbender) => {
   return fogbender;
-}
+});
 
-export const FogbenderProvider: Component<FogbenderProviderProps> = props => {
-  return (
-    <FogbenderContext.Provider value={props.fogbender}>{props.children}</FogbenderContext.Provider>
-  );
-};
+customElement<FogbenderProviderProps>(
+  "fogbender-provider",
+  { fogbender: undefined, children: undefined },
+  props => {
+    const defaultFogbender = props.fogbender || createNewFogbender();
+
+    provide(fogbenderContext, defaultFogbender);
+
+    return [...(props.children as JSX.Element[])];
+  }
+);
+
+export { fogbenderContext };
