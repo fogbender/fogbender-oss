b6371b870c860f88cf2ba4d79a9ff6fb8f817a5a
karan-nakra <karannakra1@gmail.com>
2022-10-16 18:36:52 +0530

    add unreadbadge for fogbender vue

--- packages/fogbender-vue/src/index.ts
+++ packages/fogbender-vue/src/index.ts
@@ -3,10 +3,12 @@ import FogbenderProvider from "./components/FogbenderProvider";
 import FogbenderFloatingWidget from "./components/FogbenderFloatingWidget";
 import FogbenderConfig from "./components/FogbenderConfig";
 import FogbenderIsConfigured from "./components/FogbenderIsConfigured";
+import FogbenderUnreadBadge from "./components/FogbenderUnreadBadge";
 export {
   FogbenderSimpleWidget,
   FogbenderProvider,
   FogbenderFloatingWidget,
   FogbenderConfig,
   FogbenderIsConfigured,
+  FogbenderUnreadBadge,
 };
