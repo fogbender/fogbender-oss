c0b8b358736193c1270ad218565e2561693f7bc2
JLarky <jlarky@gmail.com>
2022-12-03 18:03:54 -0700

    use common qgp config in astro

--- packages/qgp/src/index.ts
+++ packages/qgp/src/index.ts
@@ -1,3 +1,53 @@
+import { defineConfig, UserConfig, mergeConfig } from "vite";
+import envCompatible0 from "vite-plugin-env-compatible";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// javascript modules were a mistake
+const envCompatible: typeof envCompatible0 = (envCompatible0 as any).default;
+
 export function testFunction(name: string) {
   return `Hello ${name}`;
+}
+
+export type QgpConfig<T extends UserConfig> = {
+  skipEnvCompatible?: boolean;
+  skipTsconfigPaths?: boolean;
+  vite: T;
+};
+
+export function defineCommon<T extends UserConfig>(config: QgpConfig<T>): T {
+  const plugins: UserConfig["plugins"] = [];
+  if (!config.skipEnvCompatible) {
+    plugins.push(envCompatible());
+  }
+  if (!config.skipTsconfigPaths) {
+    plugins.push(tsconfigPaths());
+  }
+  const userConfig: UserConfig = { plugins };
+  const conf = mergeConfig(config.vite, userConfig);
+  return conf as T;
+}
+
+export function defineVite<T extends UserConfig, U extends UserConfig>(
+  common: T,
+  config: U,
+  _opts?: {}
+) {
+  const conf1 = mergeConfig(
+    common,
+    defineConfig({
+      cacheDir: "node_modules/.qgp",
+    })
+  );
+  const conf2 = mergeConfig(conf1, config);
+  return conf2 as U;
+}
+
+export function defineAstro<T extends UserConfig, U extends UserConfig>(
+  common: T,
+  config: U,
+  _opts?: {}
+) {
+  const conf = mergeConfig(common, config);
+  return conf as U;
 }
