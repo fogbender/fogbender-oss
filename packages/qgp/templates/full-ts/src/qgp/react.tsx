40c8b40a9de54c93a462211ce7e1c0f7d88c9be7
JLarky <jlarky@gmail.com>
2023-01-07 00:05:47 -0700

    make full CRA Astro example

--- packages/qgp/templates/full-ts/src/qgp/react.tsx
+++ packages/qgp/templates/full-ts/src/qgp/react.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+import "../index.css";
+import reportWebVitals from "../reportWebVitals";
 import ReactSPA from "../App";
 
 export const App = () => {
-  return <ReactSPA />;
+  return (
+    <React.StrictMode>
+      <ReactSPA />
+    </React.StrictMode>
+  );
 };
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
