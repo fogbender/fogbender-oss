87e9905e8d0485a00d56c6620ed9bbe6a201ea92
JLarky <jlarky@gmail.com>
2023-01-07 12:07:24 -0700

    update qgp-ts template

--- packages/qgp/templates/qgp-ts/src/qgp.tsx
+++
@@ -1,6 +0,0 @@
-window.process = window.process || { env: { NODE_ENV: "production" } };
-window.process.env = import.meta.env || {};
-import ReactDOM from "react-dom";
-import { App } from "./components/ReactSPA";
-
-ReactDOM.render(<App />, document.getElementById("root")!);
