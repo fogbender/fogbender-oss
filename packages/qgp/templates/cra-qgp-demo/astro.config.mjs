d1ca2b3b9d542f450f51f57b86503bf9a17e7b8b
JLarky <jlarky@gmail.com>
2023-02-10 11:20:00 -0700

    cra-qgp-demo template to turn your existing CRA into astro-vite-cra-ts template

---
+++ packages/qgp/templates/cra-qgp-demo/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import { defineAstro } from 'qgp';
+
+import { common } from './qgp.config.mjs';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [react()],
+	vite: defineAstro(common, {}),
+	server: { port: 3000 },
+});
