a79aa5621d72d0bcf5ad4cc9640f563c2d348f80
JLarky <jlarky@gmail.com>
2023-06-08 19:10:09 -0600

    stop importing storefront/src/ui/Landing.tsx file

--- storefront/src/qgp/react-ssr.tsx
+++
@@ -1,9 +0,0 @@
-// tslint:disable:ordered-imports
-import "./env";
-import "../trackjs";
-import "../styles/tailwind.css";
-import { Landing } from "../ui/Landing";
-
-export const App = () => {
-  return <Landing />;
-};
