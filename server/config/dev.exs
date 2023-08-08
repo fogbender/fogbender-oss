81d8e415976943aa1ac644c08795061075b01cef
JLarky <jlarky@gmail.com>
2023-08-08 11:20:12 -0600

    do not start Bamboo.SesAdapter if INBOX_SQS_URL is not set

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -2,7 +2,11 @@ import Config
 
 config :exsync, addition_dirs: ["/priv"]
 
-config :fog, Fog.Mailer, adapter: Bamboo.SesAdapter
-# adapter: Bamboo.LocalAdapter
+config :fog, Fog.Mailer,
+  adapter:
+    if(System.get_env("INBOX_SQS_URL") in [nil, ""],
+      do: Bamboo.LocalAdapter,
+      else: Bamboo.SesAdapter
+    )
 
 config :fog, Fog.Scheduler, debug_logging: true
