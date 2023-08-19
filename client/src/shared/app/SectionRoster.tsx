698989dd9a47ca98e07442c98d969da3b39b983c
Andrei Soroker <andrei@fogbender.com>
2023-08-18 19:43:27 -0700

    Change customer room headers to black for consistency. Make customer name in roster light gray

--- client/src/shared/app/SectionRoster.tsx
+++ client/src/shared/app/SectionRoster.tsx
@@ -267,7 +267,7 @@ export const RoomItem: React.FC<{
     >
       {isAgent && (
         <div className="flex items-center space-x-1 fog:text-caption-xl truncate">
-          <div className="flex-1 flex flex-col truncate">
+          <div className="flex-1 flex flex-col truncate text-gray-400">
             {formatCustomerName(room.customerName)}
           </div>
           {isAgent && !resolved && (
