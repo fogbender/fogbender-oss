b09a64b7ff3edff84722bfae9b24ee8ca4847c34
JLarky <jlarky@gmail.com>
2023-08-30 15:49:55 -0600

    implement visitor widget

---
+++ storefront/src/ui/landing/_VisitorWidget.tsx
@@ -0,0 +1,62 @@
+import type { APIFogbenderVisitor } from "fogbender-client/src/shared/z_types";
+import {
+  FogbenderConfig,
+  FogbenderFloatingWidget,
+  FogbenderHeadlessWidget,
+  FogbenderIsConfigured,
+  FogbenderProvider,
+} from "fogbender-react";
+import React from "react";
+import wretch from "wretch";
+
+import {
+  detectBetaEnvironment,
+  getClientUrlWithBeta,
+  getServerUrl,
+  getVersion,
+} from "../../config";
+
+const clientUrl = getClientUrlWithBeta();
+
+export const apiServer = wretch(getServerUrl(), {});
+
+export function VisitorWidget() {
+  const [visitor, setVisitor] = React.useState<APIFogbenderVisitor>();
+  React.useEffect(() => {
+    apiServer
+      .get("/public/fogbender_visitor")
+      .json<APIFogbenderVisitor>()
+      .then(x => {
+        setVisitor(x);
+      });
+  }, []);
+
+  return (
+    <div>
+      <FogbenderProvider>
+        {visitor && (
+          <FogbenderConfig
+            env={detectBetaEnvironment()}
+            clientUrl={clientUrl}
+            token={{
+              widgetId: visitor.widgetId,
+              widgetKey: "not used", // TODO: remove this
+              userPaseto: visitor.widgetPaseto,
+              // @ts-ignore
+              visitor: true,
+              versions: {
+                storefront: getVersion().debugVersion,
+              },
+            }}
+          />
+        )}
+        <FogbenderIsConfigured>
+          <FogbenderHeadlessWidget />
+          <FogbenderFloatingWidget />
+        </FogbenderIsConfigured>
+      </FogbenderProvider>
+    </div>
+  );
+}
+
+// const x: APIFogbenderVisitor;
