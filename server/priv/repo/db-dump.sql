0500890d78f18292095ae9fd9f8a794109e8a135
Mikl Kurkov <mk@fogbender.com>
2023-10-06 17:27:59 +0300

    Execute rewrite_image_urls migration

--- server/priv/repo/db-dump.sql
+++ server/priv/repo/db-dump.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.8
--- Dumped by pg_dump version 14.8
+-- Dumped from database version 14.9
+-- Dumped by pg_dump version 14.9
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
