b6a30cb8e18bd35388e8c8054a4327e9086f7841
Mikl Kurkov <mk@fogbender.com>
2023-10-16 23:43:07 +0300

    Don't run test suite on rebar build

--- flake.nix
+++ flake.nix
@@ -26,7 +26,9 @@
         inherit (gitignore.lib) gitignoreSource;
         unstable = unstable.legacyPackages.${final.system};
         # TODO switch to beam_nox.packages.erlang_26 when available in nix cache
-        beamPackages = final.unstable.beam.packages.erlang_26;
+        beamPackages = final.unstable.beam.packages.erlang_26.extend(new: old: {
+          rebar3 = old.rebar3.overrideAttrs{ doCheck = false; };
+        });
         mkMixDeps = final.callPackage ./nix/lib/mk-mix-deps.nix { };
         fogbender = final.callPackage ./nix/lib/fogbender.nix {  };
       };
