c461c15fa38b15e12188fc03099dde31c7634c66
JLarky <jlarky@gmail.com>
2023-11-02 22:49:33 -0600

    let's add some anys

--- storefront/src/env.d.ts
+++ storefront/src/env.d.ts
@@ -1,8 +1,4 @@
 /// <reference path="../.astro/types.d.ts" />
-//https://docs.astro.build/en/guides/integrations-guide/image/#picture-
-
-/// <reference types="@astrojs/image/client" />
-
 declare module "smartypants" {
   export function smartypants(text: string, attr?: number): string;
 }
