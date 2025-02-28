a03349c7b34e1a9d3d8d40137aa9ead25001aece
Andrei Soroker <andrei@fogbender.com>
2025-02-27 21:09:42 -0800

    Remove compat mode from eslint.config.mjs

--- packages/fogbender/eslint.config.mjs
+++ packages/fogbender/eslint.config.mjs
@@ -1,68 +1,67 @@
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import eslintPluginStandard from "eslint-plugin-standard";
+import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
 
-export default [{
+export default [
+  {
     ignores: [
-        "eslint.config.mjs",
-        "**/build/",
-        "**/dist/",
-        "**/node_modules/",
-        "**/.snapshots/",
-        "**/*.min.js",
+      "eslint.config.mjs",
+      "**/build/",
+      "**/dist/",
+      "**/node_modules/",
+      "**/.snapshots/",
+      "**/*.min.js",
     ],
-}, ...compat.extends(
-    // "standard",
-    // "standard-react",
-    "plugin:prettier/recommended",
-    "prettier/standard",
-    "prettier/react",
-    "plugin:@typescript-eslint/eslint-recommended"
-), {
+  },
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-        },
+      globals: {
+        ...globals.node,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "commonjs",
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: "commonjs",
 
-        parserOptions: {
-            ecmaFeatures: {
-                legacyDecorators: true,
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          legacyDecorators: true,
+          jsx: true,
         },
+      },
+    },
+
+    files: ["**/*.{ts,tsx}"],
+
+    plugins: {
+      eslintPluginStandard,
+      tsEslintPlugin,
+      react,
+      prettier,
     },
 
     settings: {
-        react: {
-            version: "17",
-        },
+      react: {
+        version: "17",
+      },
     },
 
     rules: {
-        "no-useless-return": 0,
-        "react/no-unknown-property": 0,
-        "space-before-function-paren": 0,
-        "react/prop-types": 0,
-        "react/jsx-handler-names": 0,
-        "react/jsx-fragments": 0,
-        "react/no-unused-prop-types": 0,
-        "no-unused-vars": 0,
-        "react/react-in-jsx-scope": 0,
-        "import/export": 0,
+      "no-useless-return": 0,
+      "react/no-unknown-property": 0,
+      "space-before-function-paren": 0,
+      "react/prop-types": 0,
+      "react/jsx-handler-names": 0,
+      "react/jsx-fragments": 0,
+      "react/no-unused-prop-types": 0,
+      "no-unused-vars": 0,
+      "react/react-in-jsx-scope": 0,
+      "import/export": 0,
     },
-}];
+  },
+];
