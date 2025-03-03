cca7fe88207877fa2911d4be0d3f06b1045ec19c
Andrei Soroker <andrei@fogbender.com>
2025-03-02 17:55:31 -0800

    Upgrade eslint in packages/fogbender-react

---
+++ packages/fogbender-react/eslint.config.mjs
@@ -0,0 +1,60 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import js from "@eslint/js";
+
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import eslintPluginStandard from "eslint-plugin-standard";
+import tsEslintPlugin from "@typescript-eslint/eslint-plugin";
+import jestPlugin from "eslint-plugin-jest";
+
+export default [
+  {
+    ignores: ["**/build/", "**/dist/", "**/node_modules/", "**/.snapshots/", "**/*.min.js"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...jestPlugin.environments.globals.globals,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: "commonjs",
+
+      parserOptions: {
+        ecmaFeatures: {
+          legacyDecorators: true,
+          jsx: true,
+        },
+      },
+    },
+
+    files: ["**/*.{ts,tsx,js}"],
+
+    plugins: {
+      eslintPluginStandard,
+      tsEslintPlugin,
+      react,
+      prettier,
+      jest: jestPlugin,
+    },
+
+    settings: {
+      react: {
+        version: "18",
+      },
+    },
+
+    rules: {
+      "space-before-function-paren": 0,
+      "react/prop-types": 0,
+      "react/jsx-handler-names": 0,
+      "react/jsx-fragments": 0,
+      "react/no-unused-prop-types": 0,
+      "import/export": 0,
+    },
+  },
+];
