6d7624c3018e9b687102fb928bc766ee30c3a014
Andrei Soroker <andrei@fogbender.com>
2025-03-23 16:15:29 -0700

    Fix React example

--- examples/react/src/index.tsx
+++ examples/react/src/index.tsx
@@ -7,13 +7,13 @@ const MyFogbender = () => {
   const token = {
     widgetId: "dzAwMTQ5OTEzNjgyNjkwNzA3NDU2",
     customerId: "org123",
-    customerName: "Org Name",
+    customerName: "Customer Firm",
     userId: "example_PLEASE_CHANGE",
     userEmail: "user@example.com",
     userHMAC: "04b7c1aab187a84bfa3160b99c100df08c78b3a1e25884fc13d8d72a9b96ddc3",
-    userName: "User Name",
+    userName: "Customer User",
     userAvatarUrl:
-      "https://user-images.githubusercontent.com/7026/108277328-19c97700-712e-11eb-96d6-7de0c98c9e3d.png", // optional
+      "https://fogbender-blog.s3.us-east-1.amazonaws.com/fogbender-cardinal-closeup.png", // optional
   };
 
   return <FogbenderSimpleWidget clientUrl={clientUrl} token={token} />;
@@ -25,12 +25,22 @@ const App = () => {
     setShow(!show);
   };
   return (
-    <>
-      <button id="button" onClick={hide}>
-        {show ? "Unmount" : "Remount"}
-      </button>
-      {show && <MyFogbender />}
-    </>
+    <div style={{ display: "flex", flex: "1" }}>
+      <div style={{ display: "flex", flex: "1", flexDirection: "column" }}>
+        <button
+          style={{
+            height: "24px",
+            border: "none",
+            borderBottom: "1px dashed gray",
+          }}
+          id="button"
+          onClick={hide}
+        >
+          {show ? "Unmount" : "Remount"}
+        </button>
+        {show && <MyFogbender />}
+      </div>
+    </div>
   );
 };
 
