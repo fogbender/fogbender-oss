baaf218edd86e52a83a8d1a40bbb4c0ac759c3bd
Andrei Soroker <andrei@fogbender.com>
2025-03-24 13:36:38 -0700

    All together now

--- examples/vue/vue.config.js
+++ examples/vue/vue.config.js
@@ -3,5 +3,14 @@ module.exports = {
     allowedHosts: "all",
     hot: false,
     liveReload: false,
+    client: {
+      webSocketURL: {
+        protocol: 'wss',
+        hostname: process.env.CODESANDBOX_HOST || "localhost",
+        port: 443,
+        pathname: '/ws',
+      },
+      overlay: false,
+    },
   },
 };
