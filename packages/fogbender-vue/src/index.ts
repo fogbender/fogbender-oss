da51cae51a39052c7092a3db4ba707f7d82b6f6c
Andrei Soroker <andrei@fogbender.com>
2025-04-07 20:37:44 -0700

    Add aliases for Roomy/Floaty in fogbender-vue

--- packages/fogbender-vue/src/index.ts
+++ packages/fogbender-vue/src/index.ts
@@ -4,10 +4,17 @@ import FogbenderFloatingWidget from "./components/FogbenderFloatingWidget";
 import FogbenderConfig from "./components/FogbenderConfig";
 import FogbenderIsConfigured from "./components/FogbenderIsConfigured";
 import FogbenderUnreadBadge from "./components/FogbenderUnreadBadge";
+
+// ✅ Aliases
+const FogbenderSimpleRoomyWidget = FogbenderSimpleWidget;
+const FogbenderSimpleFloatyWidget = FogbenderFloatingWidget;
+
 export {
   FogbenderSimpleWidget,
-  FogbenderProvider,
+  FogbenderSimpleRoomyWidget,
   FogbenderFloatingWidget,
+  FogbenderSimpleFloatyWidget,
+  FogbenderProvider,
   FogbenderConfig,
   FogbenderIsConfigured,
   FogbenderUnreadBadge,
