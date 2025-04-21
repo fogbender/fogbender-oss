f1152d984b4b73c929967709816e090ee887d065
Andrei Soroker <andrei@fogbender.com>
2025-04-20 23:51:49 -0700

    Next example - WIP

---
+++ examples/next/src/app/FogbenderWidget.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { FogbenderSimpleWidget, type Token as FogbenderToken } from "fogbender-react";
+
+export default function FogbenderWidget() {
+  const clientUrl = "https://main--fb-client.netlify.app";
+  const [token, setToken] = useState<FogbenderToken | null>(null);
+
+  useEffect(() => {
+    fetch("/fogbender-jwt")
+      .then(res => res.json())
+      .then(data => {
+        const { userJWT } = data;
+        setToken({
+          widgetId: "dzAwMTQ5OTEzNjgyNjkwNzA3NDU2",
+          customerId: "org123",
+          customerName: "Customer Firm",
+          userId: "example_PLEASE_CHANGE",
+          userEmail: "user@example.com",
+          userJWT,
+          userName: "Customer User",
+          userAvatarUrl:
+            "https://fogbender-blog.s3.us-east-1.amazonaws.com/fogbender-cardinal-closeup.png",
+        });
+      })
+      .catch(console.error);
+  }, []);
+
+  if (!token) {
+    return null;
+  }
+
+  return <FogbenderSimpleWidget clientUrl={clientUrl} token={token} />;
+}
---
+++ examples/next/src/app/page.tsx
@@ -0,0 +1,9 @@
+import FogbenderWidget from "./FogbenderWidget";
+
+export default function Home() {
+  return (
+    <div className="h-full flex flex-1">
+      <FogbenderWidget />
+    </div>
+  );
+}
