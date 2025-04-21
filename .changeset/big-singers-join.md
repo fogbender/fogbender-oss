26750f641ae6c62ae9fe2488071295bca0afe7e1
Andrei Soroker <andrei@fogbender.com>
2025-04-20 20:38:43 -0700

    Try removing tsup-preset-solid to get NextJS example to build

---
+++ .changeset/big-singers-join.md
@@ -0,0 +1,5 @@
+---
+"fogbender": patch
+---
+
+Remove tsup-preset-solid
--- packages/fogbender/tsup.config.ts
+++ packages/fogbender/tsup.config.ts
@@ -1,37 +1,10 @@
 import { defineConfig } from "tsup";
-import * as preset from "tsup-preset-solid";
 
-/*
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  splitting: false,
+  external: ["solid-js/web"],
   sourcemap: true,
+  dts: true,
   clean: true,
-});
-*/
-
-const preset_options: preset.PresetOptions = {
-  entries: [
-    {
-      entry: "src/index.ts",
-    },
-  ],
-  cjs: true,
-};
-
-export default defineConfig(config => {
-  const watching = !!config.watch;
-
-  const parsed_data = preset.parsePresetOptions(preset_options, watching);
-
-  if (!watching) {
-    const package_fields = preset.generatePackageExports(parsed_data);
-
-    console.log(`\npackage.json: \n${JSON.stringify(package_fields, null, 2)}\n\n`);
-
-    preset.writePackageJson(package_fields);
-  }
-
-  return preset.generateTsupOptions(parsed_data);
 });
