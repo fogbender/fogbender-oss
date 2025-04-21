c4f50070e93216a6dd3102ac3f4a1f367a9a095e
Andrei Soroker <andrei@fogbender.com>
2025-04-20 17:36:02 -0700

    Switch to Node 22 in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "Development environment";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
   outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         isMacOS = pkgs.stdenv.isDarwin;
@@ -15,10 +15,18 @@
           packages = with pkgs; [
             # See https://github.com/NixOS/nixpkgs/issues/59209.
             bashInteractive
+            corepack_22
+            nodejs_22
           ];
-          buildInputs = with pkgs; [
-            corepack_20
-          ];
+
+          shellHook = ''
+            set -a
+
+            if [ -f "./local.env" ]; then
+              echo Sourcing local.env..
+              . ./local.env
+            fi
+          '';
         };
       }
     );
