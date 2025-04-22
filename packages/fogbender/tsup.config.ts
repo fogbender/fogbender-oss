1b565cb8c869a9dad954c00b392b825298f831c9
Andrei Soroker <andrei@fogbender.com>
2025-04-21 17:05:15 -0700

    Kill helpful comments

--- packages/fogbender/tsup.config.ts
+++ packages/fogbender/tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"], // or 'src/index.tsx' if that’s the main file
+  entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   splitting: false,
   sourcemap: true,
   clean: true,
-  dts: true, // if you're publishing types
-  target: "esnext", // or 'es2020' if you prefer
+  dts: true,
+  target: "esnext",
 });
